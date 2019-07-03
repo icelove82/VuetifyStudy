a97a3cfbd4bf8d091b0dc4006c1247b423ad2ea7
icelove82 <yunmyeonghun@gmail.com>
2019-07-03 15:29:20 +0900

    Class 8.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
     <v-toolbar flat app>
+      <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Todo</span>
         <span>Ninja</span>
@@ -11,9 +12,19 @@
         <v-icon right>exit_to_app</v-icon>
       </v-btn>
     </v-toolbar>
+
+    <v-navigation-drawer app class="indigo" v-model="drawer">
+
+    </v-navigation-drawer>
   </nav>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      drawer: false
+    };
+  }
+};
 </script>
