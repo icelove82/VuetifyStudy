7c984ef9c828eda42c2d58d3ca53190425b631be
icelove82 <yunmyeonghun@gmail.com>
2019-07-03 12:11:33 +0900

    Class 4.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
-  <HelloWorld />
+<div class="home">
+  <h1>Homepage</h1>
+  <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nesciunt eligendi unde delectus, dolor temporibus enim, necessitatibus eius officiis repudiandae nam facilis ex possimus natus tempora nihil, iste fugiat fugit!</p>
+  <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ex. Aspernatur velit in eos laudantium, sapiente vel et voluptates! Iure nesciunt harum magnam ab neque itaque ut reprehenderit similique dolores?</p>
+  <h1 class="display-4">Massive Display</h1>
+  <h4 class="display-1">Smaller</h4>
+  <p class="headline font-weight-bold">This is a headline</p>
+  <p class="subheading">This is a subheading</p>
+  <p class="caption">This is a caption</p>
+</div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    components: {
-      HelloWorld
-    }
-  }
+export default {};
 </script>
