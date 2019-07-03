432a8f1acbd8ecdfd17d811fd3740c559866c54c
icelove82 <yunmyeonghun@gmail.com>
2019-07-03 13:33:25 +0900

    Class 5.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,30 @@
 <template>
-<div class="home">
-  <h1>Homepage</h1>
-  <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nesciunt eligendi unde delectus, dolor temporibus enim, necessitatibus eius officiis repudiandae nam facilis ex possimus natus tempora nihil, iste fugiat fugit!</p>
-  <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ex. Aspernatur velit in eos laudantium, sapiente vel et voluptates! Iure nesciunt harum magnam ab neque itaque ut reprehenderit similique dolores?</p>
-  <h1 class="display-4">Massive Display</h1>
-  <h4 class="display-1">Smaller</h4>
-  <p class="headline font-weight-bold">This is a headline</p>
-  <p class="subheading">This is a subheading</p>
-  <p class="caption">This is a caption</p>
-</div>
+  <div class="home">
+    <h1>Homepage</h1>
+
+    <v-btn class="pink white--text">click me</v-btn>
+    <v-btn depressed color="pink">click me</v-btn>
+    <v-btn flat color="pink">click me</v-btn>
+
+    <v-btn depressed class="pink white--text">
+      <v-icon left>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed small class="pink white--text">
+      <v-icon left small>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed large class="pink white--text">
+      <span>email me</span>
+      <v-icon right large>email</v-icon>
+    </v-btn>
+
+    <v-btn fab depressed small dark color="purple">
+      <v-icon>favorite</v-icon>
+    </v-btn>
+  </div>
 </template>
 
 <script>
