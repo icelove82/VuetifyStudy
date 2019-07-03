bb269d429eb353e6f81d0055bff88a0851b87b7f
icelove82 <yunmyeonghun@gmail.com>
2019-07-03 13:53:40 +0900

    Class 6.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,28 +2,12 @@
   <div class="home">
     <h1>Homepage</h1>
 
-    <v-btn class="pink white--text">click me</v-btn>
-    <v-btn depressed color="pink">click me</v-btn>
-    <v-btn flat color="pink">click me</v-btn>
+    <!-- <v-btn class="hidden-md-and-down">click me</v-btn> -->
 
-    <v-btn depressed class="pink white--text">
-      <v-icon left>email</v-icon>
-      <span>email me</span>
-    </v-btn>
+    <!-- <v-btn class="hidden-md-and-up">click me</v-btn> -->
 
-    <v-btn depressed small class="pink white--text">
-      <v-icon left small>email</v-icon>
-      <span>email me</span>
-    </v-btn>
+    <v-btn class="hidden-sm-only">click me</v-btn>
 
-    <v-btn depressed large class="pink white--text">
-      <span>email me</span>
-      <v-icon right large>email</v-icon>
-    </v-btn>
-
-    <v-btn fab depressed small dark color="purple">
-      <v-icon>favorite</v-icon>
-    </v-btn>
   </div>
 </template>
 
