eedb0a1bfe0ebd3ee5e792d890b78da99c519bb5
icelove82 <yunmyeonghun@gmail.com>
2019-07-04 16:39:14 +0900

    Class 17.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,6 +3,16 @@
     <h1 class="subheading grey--text">Dashboard</h1>
 
     <v-container class="my-5">
+      <v-layout row class="mb-3">
+        <v-btn flat small color="grey" @click="sortBy('title')">
+          <v-icon left small>folder</v-icon>
+          <span class="caption text-lowercase">By project name</span>
+        </v-btn>
+        <v-btn flat small color="grey" @click="sortBy('person')">
+          <v-icon left small>person</v-icon>
+          <span class="caption text-lowercase">By person</span>
+        </v-btn>
+      </v-layout>
       <v-card flat v-for="project in projects" :key="project.text">
         <v-layout row wrap :class="`pa-3 project ${project.status}`">
           <v-flex xs12 md6>
@@ -71,6 +81,11 @@ export default {
         }
       ]
     };
+  },
+  methods: {
+    sortBy(prop) {
+      this.projects.sort((a, b) => (a[prop] < b[prop] ? -1 : 1));
+    }
   }
 };
 </script>
